a544e4a3ab4776bad697242d724a2eb9b47549b9
donggni0712 <donggni0712@naver.com>
2024-04-29 22:18:37 +0900

    feat: Make login api

--- src/main/java/com/syncd/adapter/in/web/AuthController.java
+++ src/main/java/com/syncd/adapter/in/web/AuthController.java
@@ -2,11 +2,14 @@ package com.syncd.adapter.in.web;
 
 import com.syncd.adapter.out.persistence.repository.user.UserDao;
 import com.syncd.application.port.out.persistence.user.ReadUserPort;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.servlet.view.RedirectView;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/auth")
@@ -14,12 +17,16 @@ public class AuthController {
     private final ReadUserPort readUserPort;
 
     @GetMapping("/login/google")
-    public @ResponseBody String oauthLogin(
-            Authentication authentication,
-            @AuthenticationPrincipal OAuth2User oauth) {
-
+    public RedirectView oauthLogin(Authentication authentication, HttpServletRequest request) {
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-        System.out.println("OAuth2User:" + oauth.getAttributes());
-        return readUserPort.findByEmail(oAuth2User.getAttributes().get("email").toString()).getId();
+        System.out.println("OAuth2User: " + oAuth2User.getAttributes());
+
+        String targetUrl = request.getHeader("Referer");
+        if (targetUrl == null || targetUrl.isBlank()) {
+            // 기본 URL 설정
+            targetUrl = "/";
+        }
+
+        return new RedirectView(targetUrl);
     }
 }
