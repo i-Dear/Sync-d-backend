d06c485fa4194b041617a526d0ed12e72f16d042
JalJu <dlckswn334@naver.com>
2024-04-30 21:01:55 +0900

    feat: cors

--- src/main/java/com/syncd/adapter/in/oauth/CorsConfig.java
+++ src/main/java/com/syncd/adapter/in/oauth/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true); // 내 서버가 응답을 할 때, json을 자바스크립트에서 처리할 수 있게 할지를 정함
-        config.addAllowedOrigin("*"); // 모든 ip에 응답을 허용
+        config.addAllowedOrigin("http://localhost:3000"); // 모든 ip에 응답을 허용
         config.addAllowedHeader("*"); // 모든 header 에 응답을 허용
         config.addAllowedMethod("*"); // 모든 get, post, put, delete, patch 요청을 허용
 
