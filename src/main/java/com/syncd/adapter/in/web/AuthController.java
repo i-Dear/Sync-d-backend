cd1dcb31dc5bdb62326d3f4ddf1e097e50dd310f
Dongjae Oh <53373025+donggni0712@users.noreply.github.com>
2024-05-03 02:01:49 +0900

    [#5] JWT login 구현 (#28)
    * feat: Make room service
    
    * feat: Make room api
    
    * refactor: Refactor From layer to hexagonal
    
    * refactor: Seperate usecases
    
    * docs: Write readme
    
    * fix: Typo error
    
    * feat: Make user api
    
    * refact: Refactor to hexagonal
    
    * feat: Make liveblock auth
    
    * feat: Make liveblock auth for test
    
    * feat: Make dockerfile
    
    * merge
    
    * refactor: Delete dto layer
    
    * feat: Make all crud
    
    * feat: Make config sample
    
    * feat: Modify create project dto
    
    * feat: Apply liveblock api
    
    * fix: Allow cors
    
    * feat: Make login api
    
    * ci: Modify dockerfile
    
    * feat: Make login and jwt

--- src/main/java/com/syncd/adapter/in/web/AuthController.java
+++ src/main/java/com/syncd/adapter/in/web/AuthController.java
@@ -2,31 +2,29 @@ package com.syncd.adapter.in.web;
 
 import com.syncd.adapter.out.persistence.repository.user.UserDao;
 import com.syncd.application.port.out.persistence.user.ReadUserPort;
+import com.syncd.application.service.LoginService;
+import com.syncd.dto.TokenDto;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/auth")
+@RequestMapping("/login/oauth2")
 public class AuthController {
     private final ReadUserPort readUserPort;
+    private final LoginService loginService;
 
-    @GetMapping("/login/google")
-    public RedirectView oauthLogin(Authentication authentication, HttpServletRequest request) {
-        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-        System.out.println("OAuth2User: " + oAuth2User.getAttributes());
-
-        String targetUrl = request.getHeader("Referer");
-        if (targetUrl == null || targetUrl.isBlank()) {
-            // 기본 URL 설정
-            targetUrl = "/";
-        }
-
-        return new RedirectView(targetUrl);
+    @GetMapping("/code/{registrationId}")
+    public RedirectView googleLogin(@RequestParam String code, @PathVariable String registrationId, HttpServletResponse response) {
+        TokenDto token = loginService.socialLogin(code, registrationId);
+        response.setHeader("Authorization", "Bearer " + token.accessToken());
+        return new RedirectView("http://localhost:3000");
     }
 }
--- src/main/java/com/syncd/adapter/in/web/RoomController.java
+++ src/main/java/com/syncd/adapter/in/web/RoomController.java
@@ -1,11 +1,12 @@
 package com.syncd.adapter.in.web;
 
-import com.syncd.adapter.in.oauth.PrincipalDetails;
+import com.syncd.adapter.in.oauth.JwtTokenProvider;
 import com.syncd.application.port.in.GetAllRoomsByUserIdUsecase;
 import com.syncd.application.port.in.GetAllRoomsByUserIdUsecase.*;
 import com.syncd.application.port.in.GetRoomAuthTokenUsecase;
 import com.syncd.application.port.in.GetRoomAuthTokenUsecase.*;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -16,20 +17,23 @@ import org.springframework.web.bind.annotation.*;
 public class RoomController {
     private final GetAllRoomsByUserIdUsecase getAllRoomsByUserIdUsecase;
     private final GetRoomAuthTokenUsecase getRoomAuthTokenUsecase;
-
+    private final JwtTokenProvider jwtTokenProvider;
     @GetMapping("/auth")
-    public GetRoomAuthTokenResponseDto getRoomAuthToken(@AuthenticationPrincipal PrincipalDetails principalDetails){
-        return getRoomAuthTokenUsecase.getRoomAuthToken(principalDetails.getUser().getId());
+    public GetRoomAuthTokenResponseDto getRoomAuthToken(HttpServletRequest request){
+        String token = jwtTokenProvider.resolveToken(request);
+        return getRoomAuthTokenUsecase.getRoomAuthToken(jwtTokenProvider.getUserIdFromToken(token));
     }
 
     @GetMapping("")
-    public GetAllRoomsByUserIdResponseDto getAllInfoAboutRoomsByUserId(@AuthenticationPrincipal PrincipalDetails principalDetails){
-        return getAllRoomsByUserIdUsecase.getAllRoomsByUserId(principalDetails.getUser().getId());
+    public GetAllRoomsByUserIdResponseDto getAllInfoAboutRoomsByUserId(HttpServletRequest request){
+        String token = jwtTokenProvider.resolveToken(request);
+        return getAllRoomsByUserIdUsecase.getAllRoomsByUserId(jwtTokenProvider.getUserIdFromToken(token));
     }
 
     @PostMapping("/test-auth")
-    public GetRoomAuthTokenResponseDto getRoomAuthToken(@RequestBody GetRoomAuthTokenUsecase.TestDto getRoomAuthToken,@AuthenticationPrincipal PrincipalDetails principalDetails){
-        return getRoomAuthTokenUsecase.Test(principalDetails.getUser().getId(),getRoomAuthToken.roomId());
+    public GetRoomAuthTokenResponseDto getRoomAuthToken(@RequestBody GetRoomAuthTokenUsecase.TestDto getRoomAuthToken,HttpServletRequest request){
+        String token = jwtTokenProvider.resolveToken(request);
+        return getRoomAuthTokenUsecase.Test(jwtTokenProvider.getUserIdFromToken(token),getRoomAuthToken.roomId());
 
     }
 }
