b6eddd150f63f4838541716e0d07f8d091fed8e2
KimTaeWook <soc07249@ajou.ac.kr>
2024-05-06 18:12:47 +0900

    feat : yml 파일 매핑 추가

--- src/main/java/com/syncd/adapter/in/web/AuthController.java
+++ src/main/java/com/syncd/adapter/in/web/AuthController.java
@@ -1,5 +1,7 @@
 package com.syncd.adapter.in.web;
 
+import com.syncd.AuthControllerProperties;
+import com.syncd.GoogleOAuth2Properties;
 import com.syncd.application.service.LoginService;
 import com.syncd.dto.TokenDto;
 import jakarta.servlet.http.HttpServletResponse;
@@ -12,11 +14,13 @@ import org.springframework.web.servlet.view.RedirectView;
 @RequestMapping("/login/oauth2")
 public class AuthController {
     private final LoginService loginService;
+    private final AuthControllerProperties authControllerProperties;
 
     @GetMapping("/code/{registrationId}")
     public RedirectView googleLogin(@RequestParam String code, @PathVariable String registrationId, HttpServletResponse response) {
+        String url = authControllerProperties.getRedirectUrl();
         TokenDto token = loginService.socialLogin(code, registrationId);
-        String redirectUrl = "https://syncd.i-dear.org/org/dashboard?token=" + token.accessToken();
+        String redirectUrl = url + token.accessToken();
         return new RedirectView(redirectUrl);
     }
 }
