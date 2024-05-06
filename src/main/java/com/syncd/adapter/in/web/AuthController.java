c3501c173ecd6240bb9c3ee4b43ec75416509ad9
ChanjuLee <66884406+jalju0804@users.noreply.github.com>
2024-05-06 14:43:45 +0900

    [#5] OAuth 기반 로그인 수정 및 CI 개선 (#31)
    * feat: gitignore update
    
    * feat: dev controller
    
    * feat: dockerfile update
    
    * test: ci pipeline
    
    * test: ci pipeline
    
    * test: ci pipeline
    
    * feat: gradle wapper
    
    * test: ci
    
    * test: ci
    
    * test: ci
    
    * test: ci
    
    * test: ci
    
    * test: ci
    
    * test: encode the application.yml
    
    * test: encode the application.yml
    
    * test: folder struct
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: a lot of ci
    
    test: a lot of ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci
    
    test: ci

--- src/main/java/com/syncd/adapter/in/web/AuthController.java
+++ src/main/java/com/syncd/adapter/in/web/AuthController.java
@@ -1,30 +1,22 @@
 package com.syncd.adapter.in.web;
 
-import com.syncd.adapter.out.persistence.repository.user.UserDao;
-import com.syncd.application.port.out.persistence.user.ReadUserPort;
 import com.syncd.application.service.LoginService;
 import com.syncd.dto.TokenDto;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/login/oauth2")
 public class AuthController {
-    private final ReadUserPort readUserPort;
     private final LoginService loginService;
 
     @GetMapping("/code/{registrationId}")
     public RedirectView googleLogin(@RequestParam String code, @PathVariable String registrationId, HttpServletResponse response) {
         TokenDto token = loginService.socialLogin(code, registrationId);
-        response.setHeader("Authorization", "Bearer " + token.accessToken());
-        return new RedirectView("http://localhost:3000");
+        String redirectUrl = "https://syncd.i-dear.org#token=" + token.accessToken();
+        return new RedirectView(redirectUrl);
     }
 }
