c3d9260bc1221af36be3e1341a23ff2490c6508a
xodnz <128214672+xodnz@users.noreply.github.com>
2024-05-13 22:43:45 +0900

    [#61] joinProject 구현
    Co-authored-by: KimTaeWook <soc07249@ajou.ac.kr>

--- src/main/java/com/syncd/adapter/in/web/ProjectController.java
+++ src/main/java/com/syncd/adapter/in/web/ProjectController.java
@@ -3,6 +3,7 @@ package com.syncd.adapter.in.web;
 import com.syncd.adapter.in.oauth.JwtTokenProvider;
 import com.syncd.application.port.in.*;
 import com.syncd.application.port.in.CreateProjectUsecase.*;
+import com.syncd.application.port.in.JoinProjectUsecase.*;
 import com.syncd.application.port.in.InviteUserInProjectUsecase.*;
 import com.syncd.application.port.in.WithdrawUserInProjectUsecase.*;
 import com.syncd.application.port.in.UpdateProjectUsecase.*;
@@ -20,6 +21,8 @@ public class ProjectController {
     private final GetAllRoomsByUserIdUsecase getAllRoomsByUserIdUsecase;
     private final CreateProjectUsecase createProjectUsecase;
 
+    private final JoinProjectUsecase joinProjectUsecase;
+
     private final InviteUserInProjectUsecase inviteUserInProjectUsecase;
 
     private final WithdrawUserInProjectUsecase withdrawUserInProjectUsecase;
@@ -36,31 +39,37 @@ public class ProjectController {
     @PostMapping("/create")
     public CreateProjectResponseDto createProject(HttpServletRequest request, @Valid @RequestBody CreateProjectRequestDto requestDto){
         String token = jwtTokenProvider.resolveToken(request);
-        return createProjectUsecase.createProject(jwtTokenProvider.getUserIdFromToken(token),jwtTokenProvider.getUsernameFromToken(token), requestDto.name(), requestDto.description(), requestDto.img(), requestDto.userEmails());
+        return createProjectUsecase.createProject(jwtTokenProvider.getUserIdFromToken(token), jwtTokenProvider.getUsernameFromToken(token), requestDto.name(), requestDto.description(), requestDto.img(), requestDto.userEmails());
+    }
+
+    @PostMapping("/join")
+    public JoinProjectResponseDto joinProject(HttpServletRequest request, @Valid @RequestBody JoinProjectRequestDto requestDto) {
+        String token = jwtTokenProvider.resolveToken(request);
+        return joinProjectUsecase.joinProject(jwtTokenProvider.getUserIdFromToken(token), requestDto.projectId());
     }
 
     @PostMapping("/invite")
     public InviteUserInProjectResponseDto inviteUser(HttpServletRequest request, @Valid @RequestBody InviteUserInProjectRequestDto requestDto){
         String token = jwtTokenProvider.resolveToken(request);
-        return inviteUserInProjectUsecase.inviteUserInProject(jwtTokenProvider.getUserIdFromToken(token),requestDto.projectId(),requestDto.users());
+        return inviteUserInProjectUsecase.inviteUserInProject(jwtTokenProvider.getUserIdFromToken(token), requestDto.projectId(), requestDto.users());
     }
 
     @PostMapping("/withdraw")
     public WithdrawUserInProjectResponseDto withdrawUser(HttpServletRequest request,@Valid @RequestBody WithdrawUserInProjectRequestDto requestDto){
         String token = jwtTokenProvider.resolveToken(request);
-        return withdrawUserInProjectUsecase.withdrawUserInProject(jwtTokenProvider.getUserIdFromToken(token), requestDto.projectId(),requestDto.users());
+        return withdrawUserInProjectUsecase.withdrawUserInProject(jwtTokenProvider.getUserIdFromToken(token), requestDto.projectId(), requestDto.users());
     }
 
     @PostMapping("/delete")
     public DeleteProjectResponseDto deleteProject(HttpServletRequest request,@Valid @RequestBody DeleteProjectRequestDto requestDto){
         String token = jwtTokenProvider.resolveToken(request);
-        return deleteProjectUsecase.deleteProject(jwtTokenProvider.getUserIdFromToken(token),requestDto.projectId());
+        return deleteProjectUsecase.deleteProject(jwtTokenProvider.getUserIdFromToken(token), requestDto.projectId());
     }
 
     @PostMapping("/update")
     public UpdateProjectResponseDto updateProject(HttpServletRequest request, @Valid @RequestBody UpdateProjectRequestDto requestDto){
         String token = jwtTokenProvider.resolveToken(request);
-        return updateProjectUsecase.updateProject(jwtTokenProvider.getUserIdFromToken(token),requestDto.projectId(),requestDto.projectName(),requestDto.description(),requestDto.image());
+        return updateProjectUsecase.updateProject(jwtTokenProvider.getUserIdFromToken(token), requestDto.projectId(), requestDto.projectName(), requestDto.description(), requestDto.image());
     }
 
     @PostMapping("/sync")
